680043d8b5f6b86d556b64c7b3e4fdcd7198b80e
ania.brown <ania.brown@gmail.com>
2017-10-04 10:03:08 +0100

    adding 32 node test on arcus-b

---
+++ rotation/arcus-b_CPU/35qubits_32ranksx16threads/mpiJob.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# set the number of nodes and processes per node. We are running one process on a single node
+#SBATCH --nodes=32
+#SBATCH --ntasks-per-node=1
+
+#SBATCH --mem=50Gb
+# uncomment if NUM_QUBITS - log2(NUM_NODES) > 30
+####SBATCH --mem=100Gb
+
+# set max wallclock time
+#SBATCH --time=01:30:00
+
+# set name of job
+#SBATCH --job-name QuEST
+
+# set queue
+#SBATCH --partition=compute
+
+NUM_QUBITS=35
+EXE=demo
+export OMP_NUM_THREADS=16
+
+module purge
+module load mvapich2
+
+. enable_arcus-b_mpi.sh
+
+make clean
+make
+
+mpirun $MPI_HOSTS ./$EXE $NUM_QUBITS
