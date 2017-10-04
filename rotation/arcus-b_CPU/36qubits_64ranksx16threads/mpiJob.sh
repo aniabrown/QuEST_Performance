9079798dd30bb21ed049a4e27b347d23d32209d0
ania.brown <ania.brown@gmail.com>
2017-10-04 12:34:15 +0100

    forgot to add job script

---
+++ rotation/arcus-b_CPU/36qubits_64ranksx16threads/mpiJob.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# set the number of nodes and processes per node. We are running one process on a single node
+#SBATCH --nodes=64
+#SBATCH --ntasks-per-node=1
+
+#SBATCH --mem=50Gb
+# uncomment if NUM_QUBITS - log2(NUM_NODES) > 30
+####SBATCH --mem=100Gb
+
+# set max wallclock time
+#SBATCH --time=03:30:00
+
+# set name of job
+#SBATCH --job-name QuEST
+
+# set queue
+#SBATCH --partition=compute
+#SBATCH --reservation=nqit
+
+NUM_QUBITS=36
+EXE=demo
+export OMP_NUM_THREADS=16
+
+module purge
+module load mvapich2
+
+. enable_arcus-b_mpi.sh
+
+make clean
+make
+
+mpirun $MPI_HOSTS ./$EXE $NUM_QUBITS
