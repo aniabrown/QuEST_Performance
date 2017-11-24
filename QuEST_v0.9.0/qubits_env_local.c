000daabf6ba19f84d74b5de7b46183ab982430cf
Brown <abrown15@cx2-login.ib0.cx2.hpc.ic.ac.uk>
2017-11-24 11:41:51 +0000

    weak scaling for rotations on IC cluster

--- QuEST_v0.9.0/qubits_env_local.c
+++ QuEST_v0.9.0/qubits_env_local.c
@@ -86,6 +86,8 @@ REAL getImagAmpEl(MultiQubit multiQubit, long long int index){
 
 void rotateQubit(MultiQubit multiQubit, const int rotQubit, Complex alpha, Complex beta) 
 {
+
+printf("IN LOCAL\n");
 	// all values required to update state vector lie in this rank
 	rotateQubitLocal(multiQubit, rotQubit, alpha, beta);
 }
