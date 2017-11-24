8b3ca220cc80834b78f956eaa3941eec9dda040a
ania.brown <ania.brown@gmail.com>
2017-11-24 14:08:04 +0000

    29 qubit rotations on 6TB node

---
+++ rotation/arcus-b_6TB/29qubits_1ranksx128threads/ompJob.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# set the number of nodes and processes per node. We are running one process on a single node
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+
+# set max wallclock time
+#SBATCH --time=01:30:00
+
+# set name of job
+#SBATCH --job-name QuEST
+
+# set queue
+#SBATCH --partition=mem6T
+#SBATCH --exclusive
+
+NUM_QUBITS=29
+EXE=demo
+export OMP_PROC_BIND=true
+export OMP_NUM_THREADS=128
+
+module purge
+module load intel-compilers
+
+make clean
+make
+
+./$EXE $NUM_QUBITS
