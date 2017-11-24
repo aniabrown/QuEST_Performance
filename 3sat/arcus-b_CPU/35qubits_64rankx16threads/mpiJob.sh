b5f311517a71f570e89572dbd14729f53c23be64
ania.brown <ania.brown@gmail.com>
2017-11-24 16:42:50 +0000

    35 qubit strong scaling 3sat arcus

---
+++ 3sat/arcus-b_CPU/35qubits_64rankx16threads/mpiJob.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# set the number of nodes and processes per node. We are running one process on a single node
+#SBATCH --nodes=64
+#SBATCH --ntasks-per-node=1
+
+#SBATCH --mem=50Gb
+# uncomment if NUM_QUBITS - log2(NUM_NODES) > 30
+####SBATCH --mem=100Gb
+
+# set max wallclock time
+#SBATCH --time=02:30:00
+
+# set name of job
+#SBATCH --job-name QuEST
+
+# set queue
+#SBATCH --partition=compute
+#SBATCH --reservation=nqit
+
+EXE=demo
+export OMP_NUM_THREADS=16
+
+module purge
+module load mvapich2
+
+. enable_arcus-b_mpi.sh
+
+make clean
+make
+
+mpirun $MPI_HOSTS ./$EXE ../../3sat_input/q35/equ35qb.txt out.txt 1 0.78539816339 0
