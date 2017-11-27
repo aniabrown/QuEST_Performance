0a7335041935fbfecefac97a6658ce398bc03be9
ania.brown <ania.brown@gmail.com>
2017-11-27 10:46:27 +0000

    gpu timing for rotations

---
+++ rotation/arcus-b_GPU/29qubits_K40/gpuJob.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# set the number of nodes and processes per node. We are running one process on a single node
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+
+# set max wallclock time
+#SBATCH --time=00:20:00
+
+# set name of job
+#SBATCH --job-name QUEST
+
+# set queue
+#SBATCH --gres=gpu:1
+###SBATCH --partition=gpu
+#SBATCH --partition=devel-gpu
+
+NUM_QUBITS=29
+EXE=demo
+export OMP_NUM_THREADS=16
+
+module purge
+module load gpu/cuda
+
+## uncomment for reporting about GPU used
+##/system/software/arcus-b/gpu/cuda/7.0.28/samples/1_Utilities/deviceQuery/deviceQuery
+nvidia-smi
+
+make clean
+make
+
+./$EXE $NUM_QUBITS
+
+## uncomment to run a command line profiler
+##nvprof ./$EXE $NUM_QUBITS
